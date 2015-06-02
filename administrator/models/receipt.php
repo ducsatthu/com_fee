41a74e5fb70d89528d37eaaa5817843d378b56b4
DucSatThu <DucSatThu@DucSatThu-PC>
2015-06-02 13:24:04 +0700

    update base

--- administrator/models/receipt.php
+++ administrator/models/receipt.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version     1.0.0
  * @package     com_fee
@@ -6,7 +7,6 @@
  * @license     bản quyền mã nguồn mở GNU phiên bản 2
  * @author      Linh <mr.lynk92@gmail.com> - http://
  */
-
 // No direct access.
 defined('_JEXEC') or die;
 
@@ -15,111 +15,135 @@ jimport('joomla.application.component.modeladmin');
 /**
  * Fee model.
  */
-class FeeModelReceipt extends JModelAdmin
-{
-	/**
-	 * @var		string	The prefix to use with controller messages.
-	 * @since	1.6
-	 */
-	protected $text_prefix = 'COM_FEE';
+class FeeModelReceipt extends JModelAdmin {
+
+    /**
+     * @var		string	The prefix to use with controller messages.
+     * @since	1.6
+     */
+    protected $text_prefix = 'COM_FEE';
+    protected $_tbl = "`#__fee_receipt`";
+
+    /**
+     * Returns a reference to the a Table object, always creating it.
+     *
+     * @param	type	The table type to instantiate
+     * @param	string	A prefix for the table class name. Optional.
+     * @param	array	Configuration array for model. Optional.
+     * @return	JTable	A database object
+     * @since	1.6
+     */
+    public function getTable($type = 'Receipt', $prefix = 'FeeTable', $config = array()) {
+        return JTable::getInstance($type, $prefix, $config);
+    }
+
+    /**
+     * Method to get the record form.
+     *
+     * @param	array	$data		An optional array of data for the form to interogate.
+     * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
+     * @return	JForm	A JForm object on success, false on failure
+     * @since	1.6
+     */
+    public function getForm($data = array(), $loadData = true) {
+        // Initialise variables.
+        $app = JFactory::getApplication();
+
+        // Get the form.
+        $form = $this->loadForm('com_fee.receipt', 'receipt', array('control' => 'jform', 'load_data' => $loadData));
 
 
-	/**
-	 * Returns a reference to the a Table object, always creating it.
-	 *
-	 * @param	type	The table type to instantiate
-	 * @param	string	A prefix for the table class name. Optional.
-	 * @param	array	Configuration array for model. Optional.
-	 * @return	JTable	A database object
-	 * @since	1.6
-	 */
-	public function getTable($type = 'Receipt', $prefix = 'FeeTable', $config = array())
-	{
-		return JTable::getInstance($type, $prefix, $config);
-	}
+        if (empty($form)) {
+            return false;
+        }
 
-	/**
-	 * Method to get the record form.
-	 *
-	 * @param	array	$data		An optional array of data for the form to interogate.
-	 * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
-	 * @return	JForm	A JForm object on success, false on failure
-	 * @since	1.6
-	 */
-	public function getForm($data = array(), $loadData = true)
-	{
-		// Initialise variables.
-		$app	= JFactory::getApplication();
+        return $form;
+    }
 
-		// Get the form.
-		$form = $this->loadForm('com_fee.receipt', 'receipt', array('control' => 'jform', 'load_data' => $loadData));
-        
-        
-		if (empty($form)) {
-			return false;
-		}
+    /**
+     * Method to get the data that should be injected in the form.
+     *
+     * @return	mixed	The data for the form.
+     * @since	1.6
+     */
+    protected function loadFormData() {
+        // Check the session for previously entered form data.
+        $data = JFactory::getApplication()->getUserState('com_fee.edit.receipt.data', array());
 
-		return $form;
-	}
+        if (empty($data)) {
+            $data = $this->getItem();
+        }
 
-	/**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return	mixed	The data for the form.
-	 * @since	1.6
-	 */
-	protected function loadFormData()
-	{
-		// Check the session for previously entered form data.
-		$data = JFactory::getApplication()->getUserState('com_fee.edit.receipt.data', array());
+        return $data;
+    }
 
-		if (empty($data)) {
-			$data = $this->getItem();
-            
-		}
+    /**
+     * Method to get a single record.
+     *
+     * @param	integer	The id of the primary key.
+     *
+     * @return	mixed	Object on success, false on failure.
+     * @since	1.6
+     */
+    public function getItem($pk = null) {
+        if ($item = parent::getItem($pk)) {
 
-		return $data;
-	}
+            //Do any procesing on fields here if needed
+            if (!$item->title) {
+                $item->title = $this->getReceiptNext();
+            }
+            if (!$item->date) {
+                $item->date = date_format(JFactory::getDate(), 'Y-m-d');
+            }
+        }
 
-	/**
-	 * Method to get a single record.
-	 *
-	 * @param	integer	The id of the primary key.
-	 *
-	 * @return	mixed	Object on success, false on failure.
-	 * @since	1.6
-	 */
-	public function getItem($pk = null)
-	{
-		if ($item = parent::getItem($pk)) {
+        return $item;
+    }
 
-			//Do any procesing on fields here if needed
+    /**
+     * Prepare and sanitise the table prior to saving.
+     *
+     * @since	1.6
+     */
+    protected function prepareTable($table) {
+        jimport('joomla.filter.output');
 
-		}
+        if (empty($table->id)) {
 
-		return $item;
-	}
+            // Set ordering to the last item if not set
+            if (@$table->ordering === '') {
+                $db = JFactory::getDbo();
+                $db->setQuery('SELECT MAX(ordering) FROM #__fee_receipt');
+                $max = $db->loadResult();
+                $table->ordering = $max + 1;
+            }
+        }
+    }
 
-	/**
-	 * Prepare and sanitise the table prior to saving.
-	 *
-	 * @since	1.6
-	 */
-	protected function prepareTable($table)
-	{
-		jimport('joomla.filter.output');
+    protected function getReceiptNext() {
+        $year = date_format(JFactory::getDate(), 'Y');
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query1 = $db->getQuery(true);
 
-		if (empty($table->id)) {
+        $query
+                ->select('`alias`')
+                ->from('`#__fee_year`')
+                ->where("start = " . $db->quote($db->escape($year)));
 
-			// Set ordering to the last item if not set
-			if (@$table->ordering === '') {
-				$db = JFactory::getDbo();
-				$db->setQuery('SELECT MAX(ordering) FROM #__fee_receipt');
-				$max = $db->loadResult();
-				$table->ordering = $max+1;
-			}
+        $query1
+                ->select('MAX(`title`)')
+                ->from($this->_tbl)
+                ->where("`year_alias` = (" . $query . ")");
 
-		}
-	}
+
+        $db->setQuery($query1);
+        $result = $db->loadResult();
+        if ($result) {
+            return $result + 1;
+        } else {
+            return 1;
+        }
+    }
 
 }
--- administrator/models/receipts.php
+++ administrator/models/receipts.php
@@ -26,7 +26,7 @@ class FeeModelReceipts extends JModelList {
     public function __construct($config = array()) {
         if (empty($config['filter_fields'])) {
             $config['filter_fields'] = array(
-                                'id', 'a.id',
+                'id', 'a.id',
                 'ordering', 'a.ordering',
                 'state', 'a.state',
                 'created_by', 'a.created_by',
@@ -37,7 +37,6 @@ class FeeModelReceipts extends JModelList {
                 'year_alias', 'a.year_alias',
                 'date', 'a.date',
                 'paid', 'a.paid',
-
             );
         }
 
@@ -60,15 +59,15 @@ class FeeModelReceipts extends JModelList {
         $published = $app->getUserStateFromRequest($this->context . '.filter.state', 'filter_published', '', 'string');
         $this->setState('filter.state', $published);
 
-        
-		//Filtering student_alias
-		$this->setState('filter.student_alias', $app->getUserStateFromRequest($this->context.'.filter.student_alias', 'filter_student_alias', '', 'string'));
 
-		//Filtering semester_alias
-		$this->setState('filter.semester_alias', $app->getUserStateFromRequest($this->context.'.filter.semester_alias', 'filter_semester_alias', '', 'string'));
+        //Filtering student_alias
+        $this->setState('filter.student_alias', $app->getUserStateFromRequest($this->context . '.filter.student_alias', 'filter_student_alias', '', 'string'));
 
-		//Filtering year_alias
-		$this->setState('filter.year_alias', $app->getUserStateFromRequest($this->context.'.filter.year_alias', 'filter_year_alias', '', 'string'));
+        //Filtering semester_alias
+        $this->setState('filter.semester_alias', $app->getUserStateFromRequest($this->context . '.filter.semester_alias', 'filter_semester_alias', '', 'string'));
+
+        //Filtering year_alias
+        $this->setState('filter.year_alias', $app->getUserStateFromRequest($this->context . '.filter.year_alias', 'filter_year_alias', '', 'string'));
 
 
         // Load the parameters.
@@ -117,32 +116,32 @@ class FeeModelReceipts extends JModelList {
         );
         $query->from('`#__fee_receipt` AS a');
 
-        
-		// Join over the users for the checked out user
-		$query->select("uc.name AS editor");
-		$query->join("LEFT", "#__users AS uc ON uc.id=a.checked_out");
-		// Join over the user field 'created_by'
-		$query->select('created_by.name AS created_by');
-		$query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
-		// Join over the foreign key 'student_alias'
-		$query->select('#__fee_student_1887542.student_id AS students_alias_1887542');
-		$query->join('LEFT', '#__fee_student AS #__fee_student_1887542 ON #__fee_student_1887542.alias = a.student_alias');
-		// Join over the foreign key 'semester_alias'
-		$query->select('#__fee_semester_1887545.title AS semesters_title_1887545');
-		$query->join('LEFT', '#__fee_semester AS #__fee_semester_1887545 ON #__fee_semester_1887545.alias = a.semester_alias');
-		// Join over the foreign key 'year_alias'
-		$query->select('#__fee_year_1887547.start AS years_title_1887547');
-		$query->join('LEFT', '#__fee_year AS #__fee_year_1887547 ON #__fee_year_1887547.alias = a.year_alias');
 
-        
+        // Join over the users for the checked out user
+        $query->select("uc.name AS editor");
+        $query->join("LEFT", "#__users AS uc ON uc.id=a.checked_out");
+        // Join over the user field 'created_by'
+        $query->select('created_by.name AS created_by');
+        $query->join('LEFT', '#__users AS created_by ON created_by.id = a.created_by');
+        // Join over the foreign key 'student_alias'
+        $query->select('#__fee_student_1887542.student_id AS students_alias_1887542');
+        $query->join('LEFT', '#__fee_student AS #__fee_student_1887542 ON #__fee_student_1887542.alias = a.student_alias');
+        // Join over the foreign key 'semester_alias'
+        $query->select('#__fee_semester_1887545.title AS semesters_title_1887545');
+        $query->join('LEFT', '#__fee_semester AS #__fee_semester_1887545 ON #__fee_semester_1887545.alias = a.semester_alias');
+        // Join over the foreign key 'year_alias'
+        $query->select('#__fee_year_1887547.start AS years_title_1887547');
+        $query->join('LEFT', '#__fee_year AS #__fee_year_1887547 ON #__fee_year_1887547.alias = a.year_alias');
 
-		// Filter by published state
-		$published = $this->getState('filter.state');
-		if (is_numeric($published)) {
-			$query->where('a.state = ' . (int) $published);
-		} else if ($published === '') {
-			$query->where('(a.state IN (0, 1))');
-		}
+
+
+        // Filter by published state
+        $published = $this->getState('filter.state');
+        if (is_numeric($published)) {
+            $query->where('a.state = ' . (int) $published);
+        } else if ($published === '') {
+            $query->where('(a.state IN (0, 1))');
+        }
 
         // Filter by search in title
         $search = $this->getState('filter.search');
@@ -151,29 +150,29 @@ class FeeModelReceipts extends JModelList {
                 $query->where('a.id = ' . (int) substr($search, 3));
             } else {
                 $search = $db->Quote('%' . $db->escape($search, true) . '%');
-                $query->where('( a.title LIKE '.$search.'  OR  #__fee_student_1887542.student_id  LIKE '.$search.'  OR  #__fee_semester_1887545.title LIKE '.$search.'  OR  #__fee_year_1887547.start LIKE '.$search.'  OR  a.date LIKE '.$search.'  OR  a.paid LIKE '.$search.' )');
+                $query->where('( a.title LIKE ' . $search . '  OR  #__fee_student_1887542.student_id  LIKE ' . $search . '  OR  #__fee_semester_1887545.title LIKE ' . $search . '  OR  #__fee_year_1887547.start LIKE ' . $search . '  OR  a.date LIKE ' . $search . '  OR  a.paid LIKE ' . $search . ' )');
             }
         }
 
-        
 
-		//Filtering student_alias
-		$filter_student_alias = $this->state->get("filter.student_alias");
-		if ($filter_student_alias) {
-			$query->where("a.student_alias = '".$db->escape($filter_student_alias)."'");
-		}
 
-		//Filtering semester_alias
-		$filter_semester_alias = $this->state->get("filter.semester_alias");
-		if ($filter_semester_alias) {
-			$query->where("a.semester_alias = '".$db->escape($filter_semester_alias)."'");
-		}
+        //Filtering student_alias
+        $filter_student_alias = $this->state->get("filter.student_alias");
+        if ($filter_student_alias) {
+            $query->where("a.student_alias = '" . $db->escape($filter_student_alias) . "'");
+        }
 
-		//Filtering year_alias
-		$filter_year_alias = $this->state->get("filter.year_alias");
-		if ($filter_year_alias) {
-			$query->where("a.year_alias = '".$db->escape($filter_year_alias)."'");
-		}
+        //Filtering semester_alias
+        $filter_semester_alias = $this->state->get("filter.semester_alias");
+        if ($filter_semester_alias) {
+            $query->where("a.semester_alias = '" . $db->escape($filter_semester_alias) . "'");
+        }
+
+        //Filtering year_alias
+        $filter_year_alias = $this->state->get("filter.year_alias");
+        if ($filter_year_alias) {
+            $query->where("a.year_alias = '" . $db->escape($filter_year_alias) . "'");
+        }
 
 
         // Add the list ordering clause.
@@ -188,75 +187,72 @@ class FeeModelReceipts extends JModelList {
 
     public function getItems() {
         $items = parent::getItems();
-        
-		foreach ($items as $oneItem) {
 
-			if (isset($oneItem->student_alias)) {
-				$values = explode(',', $oneItem->student_alias);
+        foreach ($items as $oneItem) {
 
-				$textValue = array();
-				foreach ($values as $value){
-					$db = JFactory::getDbo();
-					$query = $db->getQuery(true);
-					$query
-							->select($db->quoteName('student_id'))
-							->from('`#__fee_student`')
-							->where($db->quoteName('alias') . ' = '. $db->quote($db->escape($value)));
-					$db->setQuery($query);
-					$results = $db->loadObject();
-					if ($results) {
-						$textValue[] = $results->student_id;
-					}
-				}
+            if (isset($oneItem->student_alias)) {
+                $values = explode(',', $oneItem->student_alias);
 
-			$oneItem->student_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->student_alias;
+                $textValue = array();
+                foreach ($values as $value) {
+                    $db = JFactory::getDbo();
+                    $query = $db->getQuery(true);
+                    $query
+                            ->select($db->quoteName('student_id'))
+                            ->from('`#__fee_student`')
+                            ->where($db->quoteName('alias') . ' = ' . $db->quote($db->escape($value)));
+                    $db->setQuery($query);
+                    $results = $db->loadObject();
+                    if ($results) {
+                        $textValue[] = $results->student_id;
+                    }
+                }
 
-			}
+                $oneItem->student_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->student_alias;
+            }
 
-			if (isset($oneItem->semester_alias)) {
-				$values = explode(',', $oneItem->semester_alias);
+            if (isset($oneItem->semester_alias)) {
+                $values = explode(',', $oneItem->semester_alias);
 
-				$textValue = array();
-				foreach ($values as $value){
-					$db = JFactory::getDbo();
-					$query = $db->getQuery(true);
-					$query
-							->select($db->quoteName('title'))
-							->from('`#__fee_semester`')
-							->where($db->quoteName('alias') . ' = '. $db->quote($db->escape($value)));
-					$db->setQuery($query);
-					$results = $db->loadObject();
-					if ($results) {
-						$textValue[] = $results->title;
-					}
-				}
+                $textValue = array();
+                foreach ($values as $value) {
+                    $db = JFactory::getDbo();
+                    $query = $db->getQuery(true);
+                    $query
+                            ->select($db->quoteName('title'))
+                            ->from('`#__fee_semester`')
+                            ->where($db->quoteName('alias') . ' = ' . $db->quote($db->escape($value)));
+                    $db->setQuery($query);
+                    $results = $db->loadObject();
+                    if ($results) {
+                        $textValue[] = $results->title;
+                    }
+                }
 
-			$oneItem->semester_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->semester_alias;
+                $oneItem->semester_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->semester_alias;
+            }
 
-			}
+            if (isset($oneItem->year_alias)) {
+                $values = explode(',', $oneItem->year_alias);
 
-			if (isset($oneItem->year_alias)) {
-				$values = explode(',', $oneItem->year_alias);
+                $textValue = array();
+                foreach ($values as $value) {
+                    $db = JFactory::getDbo();
+                    $query = $db->getQuery(true);
+                    $query
+                            ->select($db->quoteName('start'))
+                            ->from('`#__fee_year`')
+                            ->where($db->quoteName('alias') . ' = ' . $db->quote($db->escape($value)));
+                    $db->setQuery($query);
+                    $results = $db->loadObject();
+                    if ($results) {
+                        $textValue[] = $results->start;
+                    }
+                }
 
-				$textValue = array();
-				foreach ($values as $value){
-					$db = JFactory::getDbo();
-					$query = $db->getQuery(true);
-					$query
-							->select($db->quoteName('start'))
-							->from('`#__fee_year`')
-							->where($db->quoteName('alias') . ' = '. $db->quote($db->escape($value)));
-					$db->setQuery($query);
-					$results = $db->loadObject();
-					if ($results) {
-						$textValue[] = $results->start;
-					}
-				}
-
-			$oneItem->year_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->year_alias;
-
-			}
-		}
+                $oneItem->year_alias = !empty($textValue) ? implode(', ', $textValue) : $oneItem->year_alias;
+            }
+        }
         return $items;
     }
 
