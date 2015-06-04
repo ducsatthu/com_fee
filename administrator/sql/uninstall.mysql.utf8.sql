71e7a97b4b8a6587733710671fd9fdc966e6c71c
DucSatThu <DucSatThu@DucSatThu-PC>
2015-06-04 21:55:40 +0700

    update remove version

--- administrator/sql/uninstall.mysql.utf8.sql
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `#__fee_student`;
 DROP TABLE IF EXISTS `#__fee_course`;
 DROP TABLE IF EXISTS `#__fee_department`;
 DROP TABLE IF EXISTS `#__fee_level`;
@@ -6,3 +5,7 @@ DROP TABLE IF EXISTS `#__fee_fee`;
 DROP TABLE IF EXISTS `#__fee_semester`;
 DROP TABLE IF EXISTS `#__fee_year`;
 DROP TABLE IF EXISTS `#__fee_receipt`;
+
+DELETE FROM `#__content_types` WHERE `#__content_types`.`type_alias` = 'com_fee.student';
+DELETE FROM `#__ucm_history` WHERE `#__ucm_history`.`ucm_type_id` IN (SELECT `type_id` FROM `#__content_types` WHERE `#__content_types`.`type_alias` = 'com_fee.student');
+DROP TABLE IF EXISTS `#__fee_student`;
