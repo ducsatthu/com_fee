50a887738153f8c98b663e8995499612d0c0c69f
DucSatThu <DucSatThu@DucSatThu-PC>
2015-06-04 17:04:22 +0700

    fix convert number

--- administrator/models/receipt.php
+++ administrator/models/receipt.php
@@ -99,7 +99,7 @@ class FeeModelReceipt extends JModelAdmin {
 
         return $item;
     }
-    
+
     public function getItemPrint($pk = null) {
         if ($item = parent::getItem($pk)) {
 
@@ -170,30 +170,218 @@ class FeeModelReceipt extends JModelAdmin {
                 $db->setQuery($query);
 
                 $year = $db->loadResult();
-                
-               
+
+
                 if (!$year) {
                     $this->setError(JText::_("COM_FEE_ERROR_YEAR_DO_NOT_EXITS"));
                     return false;
                 }
                 $item->year_title = $year;
-                
             }
-           
+
             $date = new DateTime($item->date);
             $item->day = date_format($date, 'd');
             $item->month = date_format($date, 'm');
             $item->year = date_format($date, 'Y');
-            
+
             if (!$this->convert_number_to_words((int) $item->paid)) {
                 $this->setError(JText::_("COM_FEE_ERROR_CONVER_NUMBER_TO_ROMAN"));
                 return false;
             }
             $item->paidString = $this->convert_number_to_words((int) $item->paid);
-             
         }
         return $item;
     }
+
+    public function convert_number_to_words($number) {
+
+        $hyphen = ' ';
+        $conjunction = '  ';
+        $separator = ' ';
+        $negative = 'âm ';
+        $decimal = ' phẩy ';
+        $dictionary = array(
+            0 => 'Không',
+            1 => 'Một',
+            2 => 'Hai',
+            3 => 'Ba',
+            4 => 'Bốn',
+            5 => 'Năm',
+            6 => 'Sáu',
+            7 => 'Bảy',
+            8 => 'Tám',
+            9 => 'Chín',
+            10 => 'Mười',
+            11 => 'Mười một',
+            12 => 'Mười hai',
+            13 => 'Mười ba',
+            14 => 'Mười bốn',
+            15 => 'Mười năm',
+            16 => 'Mười sáu',
+            17 => 'Mười bảy',
+            18 => 'Mười tám',
+            19 => 'Mười chín',
+            20 => 'Hai mươi',
+            30 => 'Ba mươi',
+            40 => 'Bốn mươi',
+            50 => 'Năm mươi',
+            60 => 'Sáu mươi',
+            70 => 'Bảy mươi',
+            80 => 'Tám mươi',
+            90 => 'Chín mươi',
+            100 => 'trăm',
+            1000 => 'ngàn',
+            1000000 => 'triệu',
+            1000000000 => 'tỷ',
+            1000000000000 => 'nghìn tỷ',
+            1000000000000000 => 'ngàn triệu triệu',
+            1000000000000000000 => 'tỷ tỷ'
+        );
+
+        if (!is_numeric($number)) {
+            return false;
+        }
+
+        if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+// overflow
+            trigger_error(
+                    'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING
+            );
+            return false;
+        }
+
+        if ($number < 0) {
+            return $negative . $this->convert_number_to_words(abs($number));
+        }
+
+        $string = $fraction = null;
+
+        if (strpos($number, '.') !== false) {
+            list($number, $fraction) = explode('.', $number);
+        }
+
+        switch (true) {
+            case $number < 21:
+                $string = $dictionary[$number];
+                break;
+            case $number < 100:
+                $tens = ((int) ($number / 10)) * 10;
+                $units = $number % 10;
+                $string = $dictionary[$tens];
+                if ($units) {
+                    $string .= $hyphen . $dictionary[$units];
+                }
+                break;
+            case $number < 1000:
+                $hundreds = $number / 100;
+                $remainder = $number % 100;
+                $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+                if ($remainder) {
+                    $string .= $conjunction . $this->convert_number_to_words($remainder);
+                }
+                break;
+            default:
+                $baseUnit = pow(1000, floor(log($number, 1000)));
+                $numBaseUnits = (int) ($number / $baseUnit);
+                $remainder = $number % $baseUnit;
+                $string = $this->convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+                if ($remainder) {
+                    $string .= $remainder < 100 ? $conjunction : $separator;
+                    $string .= $this->convert_number_to_words($remainder);
+                }
+                break;
+        }
+
+        if (null !== $fraction && is_numeric($fraction)) {
+            $string .= $decimal;
+            $words = array();
+            foreach (str_split((string) $fraction) as $number) {
+                $words[] = $dictionary[$number];
+            }
+            $string .= implode(' ', $words);
+        }
+
+        return $string;
+    }
+
+    public function roman2number($roman) {
+        $conv = array(
+            array("letter" => 'I', "number" =>
+                1),
+            array("letter" => 'V', "number" => 5),
+            array("letter" => 'X', "number" => 10),
+            array("letter" => 'L', "number" => 50),
+            array("letter" => 'C', "number" => 100),
+            array("letter" => 'D', "number" => 500),
+            array("letter" => 'M', "number" => 1000),
+            array("letter" => 0, "number" => 0)
+        );
+        $arabic = 0;
+        $state = 0;
+        $sidx = 0;
+        $len = strlen($roman);
+
+        while ($len >= 0) {
+            $i = 0;
+            $sidx = $len;
+
+            while ($conv[$i]['number'] > 0) {
+                if (strtoupper(@$roman[$sidx]) == $conv[$i]['letter']) {
+                    if ($state > $conv[$i]['number']) {
+                        $arabic -= $conv[$i]['number'];
+                    } else {
+                        $arabic += $conv[$i]['number'];
+                        $state = $conv[$i]['number'];
+                    }
+                }
+                $i++;
+            }
+
+            $len--;
+        }
+
+        return ( $arabic);
+    }
+
+    public function number2roman($num, $isUpper = true) {
+        $n = intval($num);
+        $res = '';
+
+        /*         * * roman_numerals array ** */
+        $roman_numerals = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        );
+
+        foreach ($roman_numerals as $roman => $number) {
+            /*             * * divide to get matches ** */
+            $matches = intval($n / $number);
+
+            /*             * * assign the roman char * $matches ** */
+            $res .= str_repeat($roman, $matches);
+
+            /*             * * substract from the number ** */
+            $n = $n % $number;
+        }
+
+        /*         * * return the res ** */
+        if ($isUpper)
+            return $res;
+        else
+            return strtolower($res);
+    }
+
     /**
      * Prepare and sanitise the table prior to saving.
      *
@@ -223,7 +411,7 @@ class FeeModelReceipt extends JModelAdmin {
                 ->select('MAX(`title`)')
                 ->from($this->_tbl)
                 ->where("DATE_FORMAT(`date`,'%Y') = DATE_FORMAT(CURRENT_DATE,'%Y')");
-        
+
         $db->setQuery($query);
         $result = $db->loadResult();
         if ($result) {
