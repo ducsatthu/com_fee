a135c436d2cb4b8de8759aa6f1f25d19eebca8eb
DucSatThu <DucSatThu@DucSatThu-PC>
2015-08-14 13:01:29 +0700

    upload

--- administrator/controller.php
+++ administrator/controller.php
@@ -45,24 +45,24 @@ class FeeController extends JControllerLegacy {
 
         try {
             if (($session->get('filter_level_alias'))) {
-                if (empty($input->get('filter_level_alias'))) {
+                if (!($input->get('filter_level_alias'))) {
                     JFactory::getApplication()->input->set('filter_level_alias', $session->get('filter_level_alias'));
                 }
             }
             if (($session->get('filter_department_alias'))) {
-                if (empty($input->get('filter_department_alias'))) {
+                if (!($input->get('filter_department_alias'))) {
                     JFactory::getApplication()->input->set('filter_department_alias', $session->get('filter_department_alias'));
                 }
             }
 
             if (($session->get('filter_course_alias'))) {
-                if (empty($input->get('filter_course_alias'))) {
+                if (!($input->get('filter_course_alias'))) {
                     JFactory::getApplication()->input->set('filter_course_alias', $session->get('filter_course_alias'));
                 }
             }
 
             if (($session->get('filter_year_alias'))) {
-                if (empty($input->get('filter_year_alias'))) {
+                if (!($input->get('filter_year_alias'))) {
                     JFactory::getApplication()->input->set('filter_year_alias', $session->get('filter_year_alias'));
                 }
             }
