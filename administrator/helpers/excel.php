00e1de43a7f16b887a02f45f1f24d50695ed5b4a
DucSatThu <DucSatThu@DucSatThu-PC>
2015-09-22 16:04:27 +0700

    fix excel

--- administrator/helpers/excel.php
+++ administrator/helpers/excel.php
@@ -6,7 +6,7 @@ jimport('phpexcel.library.PHPExcel');
 class FeeHelperExcel {
 
     public static function readExcel($path) {
-        
+
         $inputFileType = 'Excel5';
         $inputFileName = $path;
         $objReader = PHPExcel_IOFactory::createReader($inputFileType);
@@ -22,16 +22,24 @@ class FeeHelperExcel {
             $department = $sheet->getCellByColumnAndRow(4, $row)->getValue();
             $course = $sheet->getCellByColumnAndRow(8, $row)->getValue();
 
-            if ($row == 4 || $department == $sheet->getCellByColumnAndRow(4, $row - 1)->getValue() && $course == $sheet->getCellByColumnAndRow(8, $row - 1)->getValue()) {
+            if ($department && $course) {
+
+
                 @$objStudent[$i]->department = $department;
                 @$objStudent[$i]->course = $course;
                 @$objStudent[$i]->student[$j]->name = $sheet->getCellByColumnAndRow(1, $row)->getValue() . " " . $sheet->getCellByColumnAndRow(2, $row)->getValue();
                 @$objStudent[$i]->student[$j]->birthday = $sheet->getCellByColumnAndRow(3, $row)->getValue();
                 @$objStudent[$i]->student[$j]->idstudent = $sheet->getCellByColumnAndRow(5, $row)->getValue() . $sheet->getCellByColumnAndRow(6, $row)->getValue() . $sheet->getCellByColumnAndRow(7, $row)->getValue();
+
                 $j++;
-            } else {
-                $i++;
-                $j = 0;
+            }
+
+            /* @var $sheet type */
+            if ($sheet->getCellByColumnAndRow(4, $row - 1)->getValue() && $sheet->getCellByColumnAndRow(8, $row - 1)->getValue()) {
+                if ($department != $sheet->getCellByColumnAndRow(4, $row - 1)->getValue() && $course != $sheet->getCellByColumnAndRow(8, $row - 1)->getValue()) {
+                    $i+=2;
+                    $j = 0;
+                }
             }
         }
         return json_encode($objStudent);
